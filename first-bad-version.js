d1abd96176b07b5f39c78cd1929321e4f1ee34f9
nurgitlab <nurbek.math@gmail.com>
2023-01-21 01:14:07 +0300

    solved: first-bad-version

---
+++ first-bad-version.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    for (let i = 1; i <=n; i++) {
+      if (isBadVersion(i)) {return i}
+    }
+  };
+};
