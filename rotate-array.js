ab167c03ee9bccd88e755915536e43559aec01bb
nurgitlab <nurbek.math@gmail.com>
2023-01-21 21:42:56 +0300

    solved: const rotate = function(nums, k) {

---
+++ rotate-array.js
@@ -0,0 +1,11 @@
+const rotate = function(nums, k) {
+  k%=nums.length
+  let a = [...nums]
+  a = [...a.slice(nums.length - k), ...a.slice(0, nums.length - k)]
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = a[i]
+  }
+
+  return nums
+}
