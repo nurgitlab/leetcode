a495e8f6c7a3bd4e58683846eda362b4e4d13a10
nurgitlab <nurbek.math@gmail.com>
2023-03-13 21:34:51 +0300

    solved: sleep

---
+++ sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res()
+    }, millis)
+  })
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
