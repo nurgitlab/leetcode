e7ae2d46a9cb1804be635f1b745b1805dd72a3c1
nurgitlab <nurbek.math@gmail.com>
2023-05-25 21:55:25 +0300

    solved: remove-all-adjacent-duplicates-in-string

---
+++ remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,13 @@
+let removeDuplicates = function(s) {
+  let stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack[stack.length - 1] === s[i]) {
+      stack.pop()
+    } else {
+      stack.push(s[i])
+    }
+  }
+
+  return stack.join('')
+}
