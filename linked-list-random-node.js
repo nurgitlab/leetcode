47b9ed429c2656ea75803fb37f8dac3a64658a94
nurgitlab <nurbek.math@gmail.com>
2023-08-13 19:38:17 +0300

    solved: linked-list-random-node

---
+++ linked-list-random-node.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ */
+var Solution = function(head) {
+  this.arr = []
+
+  while (head) {
+    this.arr.push(head.val)
+    head = head.next
+  }
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.getRandom = function() {
+  let r = Math.floor(Math.random() * this.arr.length)
+
+  return this.arr[r]
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
